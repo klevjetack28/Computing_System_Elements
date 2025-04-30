b8d44ddbed8bde761a0dbffc511f77fd7316c525
Lukas Tackett <112716986+klevjetack28@users.noreply.github.com>
2025-04-30 06:25:42 -0700

    Testing project 4

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,112 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// Sizes
+	@256
+	D=A
+	@rows
+	M=D
+	@32
+	D=A
+	@cols
+	M=D	
+
+// Indexes
+	@rowIndex
+	M=0
+	@colIndex
+	M=0
+
+// Color
+	@0
+	D=M
+	@color
+	M=D
+
+// Start loop
+(LOOP)
+
+	// Set color to white
+	@-1
+	D=M
+	@color
+	M=D
+
+	// Check key is pressed
+	@KBD
+	D=M
+	@ROWS
+	D;JEQ
+
+	// Set color to white
+	@0
+	D=M
+	@color
+	M=D
+
+	@rowPos
+	M=0
+
+(ROWS)
+	// If (rowIndex == rows) goto END_ROWS
+	@rowIndex
+	D=M
+	@rows
+	D=D-M
+	@END_ROWS
+	D;JEQ
+
+(COLS)	
+	// If (colIndex == cols) goto END_COLS
+	@colIndex
+	D=M
+	@cols
+	D=D-M
+	@END_COLS
+	D;JEQ
+
+	// Turn column index black
+	@rowPos
+	D=M
+	@colIndex
+	D=D+M
+	@SCREEN
+	A=D+A
+	M=-1
+
+	// colIndex = colIndex + 1
+	@colIndex
+	M=M+1
+
+	// Jump to start
+	@COLS
+	0;JMP
+
+(END_COLS)
+	// Reset column index
+	@colIndex
+	M=0;
+
+	// rowPos = rowPos + 32
+	@32	
+	D=A
+	@rowPos
+	M=M+D
+
+	// rowIndex = rowIndex + 1
+	@rowIndex
+	M=M+1
+
+	// Jump to start
+	@ROWS
+	0;JMP
+
+(END_ROWS)
+	// Reset row index
+	@rowIndex
+	M=0;		
+
+(CONTINUE)
+	// Jump to LOOP
+	@LOOP
+	0;JMP
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,37 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// Initialize i to 0
+	@i
+	M=0
+	@R2
+	M=0
+// Start Looping
+(MULT)
+	// If (i == R0) goto END
+	@i
+	D=M
+	@R0
+	D=D-M
+	@END
+	D;JEQ
+
+	// Start Multiplication
+	@R1
+	D=M
+	@R2
+	M=M+D
+	
+	// i = i + 1
+	@i
+	M=M+1
+
+	// Goto loop
+	@MULT
+	0;JMP
+(END)
+	@END
+	0;JMP
+			
+
+
